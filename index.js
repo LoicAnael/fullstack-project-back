8cadacfdc87a4c0e6039bc12f2e45c2250ae3d66
LoicAnael <loicanaelbdf@gmail.com>
2023-04-21 23:06:34 +0400

    dotenv config and post middleware done

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require("express"); //1
 const cors = require("cors"); //2
 const mongoose = require("mongoose"); //3
 const app = express();
+require("dotenv").config();
 const port = 3000;
-const bodyParser = require("body-parser"); //4
+const bodyParser = require("body-parser"); // pour pouvoir loguer les donnees dans la console du terminal
 
 //schema
 const { Schema } = mongoose;
@@ -14,17 +15,14 @@ const productSchema = new Schema({
   price: Number,
 });
 const Product = mongoose.model("Product", productSchema);
-const product1 = new Product();
-product1
-  .save()
-  .then(() => console.log("nouveau produit test"))
-  .catch((err) => console.log(err));
 
 //mongoose
+const login = process.env.LOGIN;
+const password = process.env.PASSWORD;
+const db = process.env.DB;
+const uri = `mongodb+srv://${login}:${password}@cluster0.tgnmnps.mongodb.net/${db}`;
 mongoose
-  .connect(
-    "mongodb+srv://loic-anael:qW4yUjXNjxugCltv@cluster0.tgnmnps.mongodb.net/test"
-  )
+  .connect(uri)
   .then(() => console.log("vous etes connectes a mongo "))
   .catch((err) => console.log(err));
 
@@ -35,9 +33,23 @@ app.use(bodyParser.json());
 //routes
 app.post("/api/products", (req, res) => {
   console.log(req.body);
+  createProduct(req.body);
   return res.send(req.body);
 });
 
 app.listen(port, () => {
   console.log("server listen on port " + port);
 });
+
+function createProduct({ description, inStock, name, price }) {
+  const product = new Product({
+    description,
+    inStock,
+    name,
+    price,
+  });
+  product
+    .save()
+    .then(() => console.log("produit sauvegarde"))
+    .catch((err) => console.log(err));
+}
